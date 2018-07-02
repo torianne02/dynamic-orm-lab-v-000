27ec7bd302fff6e9548c94b08c528be2fb4840dc
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-02 00:50:08 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -21,4 +21,11 @@ class InteractiveRecord
     options.each do |property, value|
       self.send("#{property}=", value)
     end
+  end
+
+  def save 
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 end
